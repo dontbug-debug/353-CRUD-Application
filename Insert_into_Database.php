9f94f9a4e9c694379cad06ad864c6bfb64ee2cb3
karthaveerya <karthaj@outlook.com>
2021-11-23 21:31:14 -0600

    first commit

---
+++ Insert_into_Database.php
@@ -0,0 +1,74 @@
+<?php
+require_once("Include/DB.php");
+
+
+if (isset($_POST["Submit"])) {
+    if (!empty($_POST["name"]) && !empty($_POST["breed"])) {
+        $Name = $_POST["name"];
+        $Breed = $_POST["breed"];
+        $Age = $_POST["age"];
+        $Weight = $_POST["weight"];
+
+        $data = "Breed: " .  $Breed . "\n" . "Age: " . $Age . " yr(s)" . "\n" . "Weight: " . $Weight . " lbs.";
+        // have to add report!!
+
+        global $ConnectingDB;
+        $sql = "INSERT INTO animal_record(name, data, report) VALUES(:namE, :datA, '')";
+                $stmt = $ConnectingDB->prepare($sql);
+                $stmt->bindValue(':namE', $Name);
+                $stmt->bindValue(':datA', $data);
+                // $stmt->bindValue(':breeD', $Breed);
+                // $stmt->bindValue(':agE', $Age);
+                // $stmt->bindValue(':weighT', $Weight);
+
+        $Execute = $stmt->execute();
+        if ($Execute) {
+            echo '<span class="success">Record Has Added Successfully</span>';
+        }
+    } else {
+        echo "<span class='FieldInfoHeading'> Please add Name and Social Security Number</span>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insert Data into Database</title>
+    <link rel="stylesheet" href="Include/style.css">
+</head>
+<body>
+    <!-- center the whole form using CSS -->
+    <?php ?>
+    <div class="">
+        <form class="" action="Insert_into_Database.php" method="POST">
+            <fieldset>
+                <span class="FieldInfo">Name:</span>
+                <br>
+                <input type="text" name="name" value="">
+                <br>
+                <span class="FieldInfo">Breed:</span>
+                <br>
+                <input type="text" name="breed" value="">
+                <br>
+                <span class="FieldInfo">Age:</span>
+                <br>
+                <input type="text" name="age" value="">
+                <br>
+                <span class="FieldInfo">Weight (lbs):</span>
+                <br>
+                <input type="text" name="weight" value="">
+                <br>
+                <!-- <span class="FieldInfo">Home Address:</span>
+                <br>
+                <textarea name="HomeAddress" rows="8" cols="80"></textarea>
+                <br> -->
+                <input type="submit" name="Submit" value="Submit">
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
---
+++ View_From_Database.php
@@ -0,0 +1,106 @@
+<?php
+require_once("Include/DB.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View from Database</title>
+    <link rel="stylesheet" href="Include/style.css">
+</head>
+
+<body>
+    <h2 class="success">
+        <!-- @ - won't show error when there is no id -->
+        <?php echo @$_GET["id"]; ?>
+    </h2>
+
+    <div class="">
+        <fieldset>
+            <form class="" action="View_From_Database.php" method="GET">
+                <input type="text" name="search" value="" placeholder="Search by name or breed">
+                <input type="submit" name="searchBtn" value="Search record">
+            </form>
+        </fieldset>
+    </div>
+    <?php
+    if (isset($_GET["searchBtn"])) {
+        global $ConnectingDB;
+        $Search = $_GET["search"];
+        $sql = "SELECT * FROM animal_record WHERE name=:searcH OR breed=:searcH";
+        $stmt = $ConnectingDB->prepare($sql);
+        $stmt->bindValue(':searcH', $Search);
+        $stmt->execute();
+        while ($DataRows = $stmt->fetch()) {
+            $Id             = $DataRows["id"];
+            $Name           = $DataRows["name"];
+            $Data           = $DataRows["data"];
+            $Time           = $DataRows["time"];
+    ?>
+            <div>
+                <table width="1000" border="5" align="center">
+                    <caption>Search Result</caption>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Data</th>
+                        <th>Report</th>
+                        <th>Time</th>
+                        <th>Search Again</th>
+                    </tr>
+                    <tr>
+                        <td><?php echo $Id; ?></td>
+                        <td><?php echo $Name; ?></td>
+                        <td><?php echo $Data; ?></td>
+                        <td> </td>
+                        <td><?php echo $Time; ?></td>
+                        <td> <a href="View_From_Database.php">Search Again</a> </td>
+                    </tr>
+                </table>
+            </div>
+
+    <?php }
+    }
+    ?>
+
+    <table width="1000" border="5" align="center">
+        <caption>View from Database</caption>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Data</th>
+            <th>Report</th>
+            <th>Time</th>
+            <th>Update</th>
+            <th>Delete</th>
+        </tr>
+
+        <?php
+        global $ConnectingDB;
+        $sql = "SELECT * From animal_record";
+        $stmt = $ConnectingDB->query($sql);
+        while ($DataRows = $stmt->fetch()) {
+            $Id             = $DataRows["id"];
+            $Name           = $DataRows["name"];
+            $Data           = $DataRows["data"];
+            $Time           = $DataRows["time"];
+        ?>
+            <tr>
+                <td><?php echo $Id; ?></td>
+                <td><?php echo $Name; ?></td>
+                <td><?php echo $Data; ?></td>
+                <td> </td>
+                <td><?php echo $Time; ?></td>
+                <td> <a href="Update.php?id=<?php echo $Id; ?>">Update</a> </td>
+                <td> <a href="Delete.php?id=<?php echo $Id; ?>">Delete</a> </td>
+            </tr>
+        <?php } ?>
+    </table>
+
+</body>
+
+</html>
