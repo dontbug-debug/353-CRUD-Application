0a30672b4545348f97ac8a349f59aa3b4e5ca205
karthaveerya <60456289+dontbug-debug@users.noreply.github.com>
2021-11-28 14:34:04 -0600

    Fixed signup icon.

--- signup.php
+++ signup.php
@@ -48,6 +48,7 @@ if (isset($_POST["Submit"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Insert Data into Database</title>
     <link rel="stylesheet" href="Include/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <!-- center the whole form using CSS -->
